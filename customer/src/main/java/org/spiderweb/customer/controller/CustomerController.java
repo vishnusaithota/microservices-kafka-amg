53fc5b2cb840835b43ed6c77b8edb3dc4f9447bb
tnvrravish <vt7@njit.edu>
2023-06-17 19:24:17 -0400

    configured docker compose to run two databases and used spring profiles to connect to two different databases

--- customer/src/main/java/org/spiderweb/customer/controller/CustomerController.java
+++ customer/src/main/java/org/spiderweb/customer/controller/CustomerController.java
@@ -3,15 +3,17 @@ package org.spiderweb.customer.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.spiderweb.customer.dto.CustomerRegistrationRequest;
 import org.spiderweb.customer.service.CustomerService;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/customers")
+@RequestMapping("api/v1/customers")
 @Slf4j
 public record CustomerController(CustomerService customerService) {
 
+    @PostMapping()
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
         log.info("new Customer registration {}",customerRegistrationRequest);
         customerService.registerCustomer(customerRegistrationRequest);
