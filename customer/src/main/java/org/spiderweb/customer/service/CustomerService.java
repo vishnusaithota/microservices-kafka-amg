940384d6ca297caa41cab6f4b9420984d99bc66b
tnvrravish <vt7@njit.edu>
2023-06-17 16:32:49 -0400

    added Data Jpa and created the database we want to connect to

--- customer/src/main/java/org/spiderweb/customer/service/CustomerService.java
+++ customer/src/main/java/org/spiderweb/customer/service/CustomerService.java
@@ -2,15 +2,18 @@ package org.spiderweb.customer.service;
 
 import org.spiderweb.customer.dto.CustomerRegistrationRequest;
 import org.spiderweb.customer.model.Customer;
+import org.spiderweb.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
+
+        customerRepository.save(customer);
     }
 }
